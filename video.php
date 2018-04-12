3ce8a609404bb504a0e76738252274ed73b748ea
Tristin <34673748+Vithiar@users.noreply.github.com>
2018-04-11 20:18:29 -0500

    Update video.php

--- video.php
+++ video.php
@@ -1,3 +1,4 @@
+<!-- Tristin Harvell's video.php page -->
 <?php
 include 'inc/header.php'
 ?>
@@ -18,29 +19,33 @@ include 'inc/header.php'
         <?php echo printHeader(); ?>
         <!-- Navbar on small screens -->
 
-        <!-- First Video -->
-        <div class="video-one-container">
-            <h1 class="video-one-title">Internet Addiction Overview</h1>
-            <iframe width="420" height="315" id="video-one"
-                    src="https://www.youtube.com/watch?v=vOSYmLER664">
-            </iframe>
-        </div>
+        <article>
 
-        <!-- Second Video -->
-        <div class="video-two-container">
-            <h1 class="video-two-title">What You Miss With Internet Addiction</h1>
-            <iframe width="420" height="315" id="video-two"
-                    src="https://www.youtube.com/watch?v=TAIxb42FjwE">
-            </iframe>
-        </div>
+            <!-- First Video -->
+            <div id="video-one-container">
+                <h1 id="video-one-title">Internet Addiction Overview</h1>
+                <iframe width="420" height="315" id="video-one"
+                        src="https://www.youtube.com/watch?v=vOSYmLER664">
+                </iframe>
+            </div>
 
-        <!-- Third Video -->
-        <div class="video-three-container">
-            <h1 class="video-three-title">How Smartphones Change The Way You Think</h1>
-            <iframe width="420" height="315" id="video-three"
-                    src="https://www.youtube.com/watch?v=pWvSwX-jq7o">
-            </iframe>
-        </div>
+            <!-- Second Video -->
+            <div id="video-two-container">
+                <h1 id="video-two-title">What You Miss With Internet Addiction</h1>
+                <iframe width="420" height="315" id="video-two"
+                        src="https://www.youtube.com/watch?v=TAIxb42FjwE">
+                </iframe>
+            </div>
+
+            <!-- Third Video -->
+            <div id="video-three-container">
+                <h1 id="video-three-title">How Smartphones Change The Way You Think</h1>
+                <iframe width="420" height="315" id="video-three"
+                        src="https://www.youtube.com/watch?v=pWvSwX-jq7o">
+                </iframe>
+            </div>
+
+        </article>
 
         <!-- Footer -->
         <?php echo printFooter(); ?>

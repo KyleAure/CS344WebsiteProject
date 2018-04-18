1197fbbbbc7e9b77408cd4e06d2b09874d6a86c3
Tristin Harvell <tlharvell@gmail.com>
2018-04-17 19:05:08 -0500

    Updated video page
    Added videobody.php and removed some HTML from video.php.

--- video.php
+++ video.php
@@ -25,38 +25,14 @@ ini_set('display_errors', E_ALL);
 <!-- Script Links -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="js/header.js"></script>
+    
 <!-- Include Header -->
 <?php include_once 'inc/header.php'; ?>
-
-    <body>
-        <article>
-
-            <!-- First Video -->
-            <div id="video-one-container">
-                <h1 id="video-one-title">Internet Addiction Overview</h1>
-                <iframe width="420" height="315" id="video-one"
-                        src="https://www.youtube.com/watch?v=vOSYmLER664">
-                </iframe>
-            </div>
-
-            <!-- Second Video -->
-            <div id="video-two-container">
-                <h1 id="video-two-title">What You Miss With Internet Addiction</h1>
-                <iframe width="420" height="315" id="video-two"
-                        src="https://www.youtube.com/watch?v=TAIxb42FjwE">
-                </iframe>
-            </div>
-
-            <!-- Third Video -->
-            <div id="video-three-container">
-                <h1 id="video-three-title">How Smartphones Change The Way You Think</h1>
-                <iframe width="420" height="315" id="video-three"
-                        src="https://www.youtube.com/watch?v=pWvSwX-jq7o">
-                </iframe>
-            </div>
-
-        </article>
-
-    </body>
+    
+<!-- Body -->
+<?php include_once 'inc/videobody.php'; ?>
+    
+    <!-- Footer -->
+<?php include_once 'inc/footer.php'; ?>
 
 </html>

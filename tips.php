ef58231045958f6a6ded87043cbe157acc35d768
Nukic <bk8355tn@bk8355tnm8.workstations.winona.edu>
2018-04-18 22:24:47 -0500

    added header tabs

--- tips.php
+++ tips.php
@@ -24,6 +24,14 @@ ini_set('display_errors', E_ALL);
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="js/header.js"></script>
 <!-- Include Header -->
+<div class="topnav" id="myTopnav">
+  <a href="index.php" >Home</a>
+  <a href="history.php">History</a>
+  <a href="tips.php">Tips and Tricks</a>
+  <a href="video.php">Instructional Video</a>
+  <a href="contact.php">Contact Us</a>
+  <a href="javascript:void(0);" class="icon" onclick="updateNavClass()">&#9776;</a>
+</div>
 <header>
      <h1><Strong>
          TIPS AND TRICKS </Strong></h1>

1bc2f62ba2a7782db018f5705843f2e76df37a2b
Kyle Aure <kaure09@winona.edu>
2018-04-21 10:43:38 -0500

    Fixed Johnny's duplicate directory!

--- CS344WebsiteProject/README.md
+++
@@ -1,1 +0,0 @@
-# CS344WebsiteProject
--- CS344WebsiteProject/inc/header.php
+++
@@ -1,8 +0,0 @@
-<div class="topnav" id="myTopnav">
-  <a href="index.php" >Home</a>
-  <a href="history.php">History</a>
-  <a href="tips.php">Tips and Tricks</a>
-  <a href="video.php">Instructional Video</a>
-  <a href="contact.php">Contact Us</a>
-  <a href="javascript:void(0);" class="icon" onclick="updateNavClass()">&#9776;</a>
-</div>
